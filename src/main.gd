cb7bec5bba72c8e98316051769a4fae49e62220f
JASHiN <95284497+JASHiNSENNiN@users.noreply.github.com>
2023-10-24 09:11:17 -0700

    Tiktaktoe

---
+++ src/main.gd
@@ -0,0 +1,55 @@
+extends Node
+
+var player : int
+var grid_data : Array
+
+#initiate at start
+func _ready():
+	
+	new_game()
+
+#main process
+func _process(delta):
+	pass
+
+				
+func new_game():
+	player = 1
+	grid_data = [ 
+		[0,0,0],
+		[0,0,0],
+		[0,0,0]
+	]
+
+func _on_1_released():
+	pass # Replace with function body.
+
+func _on_2_released():
+	pass # Replace with function body.
+
+func _on_3_released():
+	pass # Replace with function body.
+
+
+func _on_4_released():
+	pass # Replace with function body.
+
+
+func _on_5_released():
+	pass # Replace with function body.
+
+
+func _on_6_released():
+	pass # Replace with function body.
+
+
+func _on_7_released():
+	pass # Replace with function body.
+
+
+func _on_8_released():
+	pass # Replace with function body.
+
+
+func _on_9_released():
+	pass # Replace with function body.
